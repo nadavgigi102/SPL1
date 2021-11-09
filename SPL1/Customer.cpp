10d87fb7167a250c925ed47ff879943402889763
nadav <nadagigi102@gmail.com>
2021-11-09 11:43:30 +0200

    update

--- SPL1/Customer.cpp
+++ SPL1/Customer.cpp
@@ -1,1 +1,23 @@
 #include "Customer.h"
+
+Customer::Customer(std::string c_name, int c_id) :name(c_name),id(c_id)
+{
+}
+
+std::string Customer::getName() const
+{
+	return this->name;
+}
+
+int Customer::getId() const
+{
+	return this->id;
+}
+
+SweatyCustomer::SweatyCustomer(std::string name, int id) : Customer(name,id)
+{
+}
+
+CheapCustomer::CheapCustomer(std::string name, int id):Customer(name,id)
+{
+}
--- SPL1/SPL1.cpp
+++ SPL1/SPL1.cpp
@@ -5,17 +5,9 @@ using namespace std;
 
 Studio* backup = nullptr;
 
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        std::cout << "usage: studio <config_path>" << std::endl;
-        return 0;
-    }
-    string configurationFile = argv[1];
-    Studio studio(configurationFile);
-    studio.start();
-    if (backup != nullptr) {
-        delete backup;
-        backup = nullptr;
-    }
-    return 0;
+int main() {
+	std::string s = "fsfs";
+	//Workout work = Workout(0, s, 50, MIXED);
+	//cout << work.getName()  ;
+	return 0;
 }
